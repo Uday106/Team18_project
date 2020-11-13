1cc143db02846020e01a964ba892cf1b852e2166
Jimun Jang <wkdwlans009@gmail.com>
2020-11-13 13:02:33 -0800

    restaurant.js

---
+++ scripts/restaurant.js
@@ -0,0 +1,45 @@
+const restaurantList = document.querySelector('.grid-container');
+
+// create element and render cafe
+function renderCafe(doc){
+    let card = document.createElement('div');
+    let name = document.createElement('p');
+    let location = document.createElement('p');
+    let reserve = document.createElement('a');
+
+
+    card.setAttribute('data-id', doc.id);
+    reserve.setAttribute('href', '');
+    name.textContent = doc.data().name;
+    location.textContent = doc.data().location;
+    reserve.textContent = 'reserve';
+
+    card.appendChild(name);
+    card.appendChild(location);
+    card.appendChild(reserve);
+    restaurantList.appendChild(card);
+
+
+
+    reserve.addEventListener('click', (e) => {
+        e.preventDefault();
+        let id = e.target.parentElement.getAttribute('data-id');
+        console.log(id);
+        console.log(db.collection('restaurants').doc(id).collection('reservation').doc('am1'));
+        db.collection('restaurants').doc(id).collection('reservation').doc('am1').update({
+            'taken': true
+        });
+    })
+};
+
+
+db.collection('restaurants').get().then((snap) => {
+    snap.docs.forEach((doc) => {
+        renderCafe(doc);
+    })
+});
+
+
+
+
+
